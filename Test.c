571226a75af2a16162631939e3dd8a98937c0726
Abdelrhman Badr <abdelrhmanezzbadr22@gmail.com>
2022-10-27 01:16:00 +0200

    Assign tasks for manr and refat

--- Test.c
+++ Test.c
@@ -1,43 +1,52 @@
 #include <stdio.h>
-#include<stdlib.h>
-#include "Priority_Queue.h"
+#include <stdlib.h>
+#include "PriorityQueue.h"
 #include "Test.h"
 
-void Test_PriorityQueue (void){
-   
-    PriorityQueue_t PQ;
-    if(!PriorityQueue_IsFull(&PQ)){
+// TODO @manar: adding printing lines
+void Test_PriorityQueue (void) {
+	
+	PriorityQueue_t PQ;
+
+    if(!PriorityQueue_IsFull(&PQ)) {
 		PriorityQueue_Initiate(&PQ);
     }
-   if(!PriorityQueue_IsFull(&PQ)){
+
+    if(!PriorityQueue_IsFull(&PQ)) {
 		PriorityQueue_Enqueue(&PQ, 20);
     }
-	if(!PriorityQueue_IsFull(&PQ)){
+
+	if(!PriorityQueue_IsFull(&PQ)) {
 		PriorityQueue_Enqueue(&PQ, 10);
     }
-	if(!PriorityQueue_IsFull(&PQ)){
+
+	if(!PriorityQueue_IsFull(&PQ)) {
 		PriorityQueue_Enqueue(&PQ, 30);
     }
-	if(!PriorityQueue_IsFull(&PQ)){
+
+	if(!PriorityQueue_IsFull(&PQ)) {
 		PriorityQueue_Enqueue(&PQ, 50);
     }
-	if(!PriorityQueue_IsFull(&PQ)){
+
+	if(!PriorityQueue_IsFull(&PQ)) {
 		PriorityQueue_Enqueue(&PQ, 40);
     }
-		PriorityQueue_Dequeue(&PQ);
+	
 	if(!PriorityQueue_IsEmpty(&PQ)){
 		PriorityQueue_Dequeue(&PQ);
     }
+
 	if(!PriorityQueue_IsEmpty(&PQ)){
 		PriorityQueue_Dequeue(&PQ);
     }
+
 	if(!PriorityQueue_IsEmpty(&PQ)){
 		PriorityQueue_Dequeue(&PQ);
     }
+
 	if(!PriorityQueue_IsEmpty(&PQ)){
 		PriorityQueue_Dequeue(&PQ);
     }   
-
-
 }
-// TODO: @ManarAbdelraouf
+
+// TODO @mRefat: adding CircularQueueArray test
