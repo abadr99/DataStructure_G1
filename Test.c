f90acc7efde9979978ca09b023d28bc169ff7b7f
Manar <manar.abdelraouf24@gmail.COM>
2022-10-26 23:43:04 +0200

    change function Test toTest_PriorityQueue ,and adding curly braces to if condition

--- Test.c
+++ Test.c
@@ -6,47 +6,37 @@
 void Test_PriorityQueue (void){
    
     PriorityQueue_t PQ;
-    if(!PriorityQueue_IsFull(&PQ))
-{
+    if(!PriorityQueue_IsFull(&PQ)){
 		PriorityQueue_Initiate(&PQ);
-}
-   if(!PriorityQueue_IsFull(&PQ))
-{
+    }
+   if(!PriorityQueue_IsFull(&PQ)){
 		PriorityQueue_Enqueue(&PQ, 20);
-}
-	if(!PriorityQueue_IsFull(&PQ))
-{
+    }
+	if(!PriorityQueue_IsFull(&PQ)){
 		PriorityQueue_Enqueue(&PQ, 10);
-}
-	if(!PriorityQueue_IsFull(&PQ))
-{
+    }
+	if(!PriorityQueue_IsFull(&PQ)){
 		PriorityQueue_Enqueue(&PQ, 30);
-}
-	if(!PriorityQueue_IsFull(&PQ))
-{
+    }
+	if(!PriorityQueue_IsFull(&PQ)){
 		PriorityQueue_Enqueue(&PQ, 50);
-}
-	if(!PriorityQueue_IsFull(&PQ))
-{
+    }
+	if(!PriorityQueue_IsFull(&PQ)){
 		PriorityQueue_Enqueue(&PQ, 40);
-}
+    }
 		PriorityQueue_Dequeue(&PQ);
-	if(!PriorityQueue_IsEmpty(&PQ))
-{
+	if(!PriorityQueue_IsEmpty(&PQ)){
 		PriorityQueue_Dequeue(&PQ);
-}
-	if(!PriorityQueue_IsEmpty(&PQ))
-{
+    }
+	if(!PriorityQueue_IsEmpty(&PQ)){
 		PriorityQueue_Dequeue(&PQ);
-}
-	if(!PriorityQueue_IsEmpty(&PQ))
-{
+    }
+	if(!PriorityQueue_IsEmpty(&PQ)){
 		PriorityQueue_Dequeue(&PQ);
-}
-	if(!PriorityQueue_IsEmpty(&PQ))
-{
+    }
+	if(!PriorityQueue_IsEmpty(&PQ)){
 		PriorityQueue_Dequeue(&PQ);
-}
+    }   
 
 
 }
