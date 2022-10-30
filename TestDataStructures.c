4d065f8961373d558438a75052d33be6ba01c909
Abdelrhman Badr <abdelrhmanezzbadr22@gmail.com>
2022-10-30 21:48:43 +0200

    Change File name

---
+++ TestDataStructures.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SonicTest.h"
+#include "TestDataStructures.h"
+
+
+int sum(int x , int y)
+{
+	return x+y;
+}
+
+TEST(SUM)
+{
+	START_TEST(SUM);
+	EXPECT_EQ(5,sum(2,3));
+	EXPECT_EQ(9,sum(4,3));  // write a woring test case
+}
---
+++ TestDataStructures.h
@@ -0,0 +1,6 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+
+TEST(SUM);
+
+#endif
