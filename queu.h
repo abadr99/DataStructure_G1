2f65e649f9ed584dbf601e6c0cc1d090e1475e98
noran97 <99215076+noran97@users.noreply.github.com>
2022-11-06 15:24:38 +0200

    Update queu.h

--- queu.h
+++ queu.h
@@ -1,37 +1,39 @@
 #ifndef QUEU_H_INCLUDED
 #define QUEU_H_INCLUDED
 #include"obj/conf.h"
-#ifdef linked
+#include "types.h"
+
+#if QUEUE_TYPE ==QUEUE_LINKED
 typedef struct node {
-    int key;
+    uint16_t key;
     struct node* next;
 }node;
 
 typedef struct queue {
-    int size;
+    uint16_t size;
     struct node *front, *rear;
 }queue_t;
- static node*newnode(int val);
+ static node*newnode(uint16_t val);
  queue_t* queue_initialize(queue_t*q);
- void queue_enqueue(queue_t* q, int val);
- void queue_dequeue(queue_t* q);
- int queue_getsize(queue_t* q);
+ void queue_enqueue(queue_t* q, uint16_t val);
+uint16_t queue_dequeue(queue_t* q);
+ uint16_t queue_GetSize(queue_t* q);
 
  #else
 typedef struct queuearray
 {
-int size;
-int arr[arraysize];
-int i;
+uint16_t size;
+uint16_t arr[arraysize];
+uint16_t i;
 }queue_t;
 void queue_initiate(queue_t*q);
-void queue_push( queue_t *q,int val);
-int queue_pop (queue_t*q);
-int queue_isEmpty(queue_t*q);
-int queue_isFull(queue_t*q);
-int queue_getfront(queue_t *q);
-int queue_getback(queue_t*q);
-int queue_getSize(queue_t*q);
+void queue_push( queue_t *q,uint16_t val);
+uint16_t queue_pop (queue_t*q);
+uint16_t queue_IsEmpty(queue_t*q);
+uint16_t queue_IsFull(queue_t*q);
+uint16_t queue_GetFront(queue_t *q);
+uint16_t queue_GetBack(queue_t*q);
+uint16_t queue_GetSize(queue_t*q);
 #endif // linked
 
 #endif // QUEU_H_INCLUDED
