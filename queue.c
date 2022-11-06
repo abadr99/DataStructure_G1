a2bbc758a87eaf9b76484372d447e5fcfb64226e
noran97 <99215076+noran97@users.noreply.github.com>
2022-11-06 18:11:18 +0200

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,24 +1,25 @@
 #include "obj\conf.h"
+#include "types.h"
 #if QUEUE_TYPE ==QUEUE_LINKED
 typedef struct node {
-    int key;
+    uint16_t key;
     struct node* next;
 }node;
 
 typedef struct queue {
-    int size;
+    uint16_t size;
     struct node *front, *rear;
 }queue_t;
 
 
-queue_t* queue_initialize(queue_t*q)
+void queue_initialize(queue_t*q)
 {
 
     q->front = q->rear = 0;
    q-> size=0;
 
 }
-void queue_enqueue(queue_t* q, int val)
+void queue_enqueue(queue_t* q, uint16_t val)
 {
     (q->size)++;
     node* newnode= (node*)malloc(sizeof(node));
@@ -31,8 +32,8 @@ void queue_enqueue(queue_t* q, int val)
     q->rear->next = newnode;
     q->rear = newnode;
 }
-int queue_dequeue(queue_t* q)
-{int val;
+uint16_t queue_dequeue(queue_t* q)
+{uint16_t val;
 (q->size)--;
     if (q->front == 0)
         return;
@@ -49,7 +50,7 @@ int queue_dequeue(queue_t* q)
     free(temp);
     return val;
 }
-int queue_GetSize(queue_t* q)
+uint16_t queue_GetSize(queue_t* q)
 {
     return q->size;
 }
@@ -61,44 +62,44 @@ int queue_GetSize(queue_t* q)
 
 typedef struct queuearray
 {
-int size;
-int arr[arraysize];
-int i;
+uint16_t size;
+uint16_t arr[arraysize];
+uint16_t i;
 }queue_t;
 
 void queue_initiate(queue_t*q)
 {q->i=0;
     q->size=0;
 }
-void queue_push( queue_t*q,int val)
+void queue_push( queue_t*q,uint16_t val)
 {
     q->arr[q->size]=val;
     (q->size)++;
 }
-int queue_pop (queue_t*q)
+uint16_t queue_pop (queue_t*q)
 {q->i++;
 q->size--;
     return q->arr[(q->i)-1];
 }
-int queue_IsEmpty(queue_t*q)
-{int val;
+uint16_t queue_IsEmpty(queue_t*q)
+{uint16_t val;
     if(q->size==0){val=1;}
     return val;
 }
-int queue_IsFull(queue_t*q)
-{int val;
+uint16_t queue_IsFull(queue_t*q)
+{uint16_t val;
     if(q->size==arraysize){val=1;}
     return val;
 }
-int queue_GetFront(queue_t *q)
+uint16_t queue_GetFront(queue_t *q)
 {
     return q->arr[q->i];
 }
-int queue_GetBack(queue_t *q)
+uint16_t queue_GetBack(queue_t *q)
 {
     return q->arr[(q->size)-1];
 }
-int queue_GetSize(queue_t*q)
+uint16_t queue_GetSize(queue_t*q)
 {
     return q->size;
 }
