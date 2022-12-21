328144c8305be4bc1e4750e05b1af9c171400e25
Mohamed_Refat13 <mo7amedrefaat2001@gmail.com>
2022-12-21 20:44:07 +0200

    Modify variable names

--- src/04_SearchingAlgrothims/SearchingAlgrothims.c
+++ src/04_SearchingAlgrothims/SearchingAlgrothims.c
@@ -1,46 +1,46 @@
 #include "STD_TYPES.h"
-static sint32_t BinaryHelper(sint32_t Array[] , sint32_t First , sint32_t Last ,sint32_t Key );
-static sint32_t BinaryHelper(sint32_t Array[] , sint32_t First , sint32_t Last ,sint32_t Key )
+static sint32_t BinaryHelper(sint32_t Array[] , sint32_t First_index , sint32_t Last_index ,sint32_t Key );
+static sint32_t BinaryHelper(sint32_t Array[] , sint32_t First_index , sint32_t Last_index ,sint32_t Key )
 {
-   sint32_t Mid = (First+Last)/2;
-   if (Last<First)
+   sint32_t Middle_index = (First_index+Last_index)/2;
+   if (Last_index<First_index)
    {
     return  NOT_FOUND ;
    }
-   if (Array[Mid]==Key)
+   if (Array[Middle_index]==Key)
    {
-    return Mid ;
+    return Middle_index ;
    }
-   else if (Array[Mid]>Key)
+   else if (Array[Middle_index]>Key)
    {
-    return BinaryHelper(Array , First , Mid-1 , Key);
-   }else if (Array[Mid]<Key)
+    return BinaryHelper(Array , First_index , Middle_index-1 , Key);
+   }else if (Array[Middle_index]<Key)
    {
-    return BinaryHelper(Array , Mid+1 , Last , Key);
+    return BinaryHelper(Array , Middle_index+1 , Last_index , Key);
    }
 }
 sint32_t SearchingAlgorithm_BinaryIterative(sint32_t Array[] , uint32_t Size,sint32_t Key)
 {
 	sint32_t LocReturn = NOT_FOUND;
-	sint32_t First = 0;
-	sint32_t Last  = Size-1;
-	uint32_t Mid   = (First+Last)/2;
-	while(Last >= First)
+	sint32_t First_index = 0;
+	sint32_t Last_index  = Size-1;
+	uint32_t Middle_index   = (First_index+Last_index)/2;
+	while(Last_index >= First_index)
 	{
-		if(Array[Mid] == Key)
+		if(Array[Middle_index] == Key)
 		{
-			LocReturn = Mid;
+			LocReturn = Middle_index;
 			break;
 
-		}else if(Array[Mid] > Key)
+		}else if(Array[Middle_index] > Key)
 		{
-			Last = Mid - 1;
-			Mid  = (First+Last)/2;
+			Last_index = Middle_index - 1;
+			Middle_index  = (First_index+Last_index)/2;
 
-		}else if(Array[Mid] < Key)
+		}else if(Array[Middle_index] < Key)
 		{
-			First = Mid + 1;
-			Mid  = (First+Last)/2;
+			First_index = Middle_index + 1;
+			Middle_index  = (First_index+Last_index)/2;
 		}
 	}
 	return LocReturn;
