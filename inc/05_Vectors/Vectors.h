8f5cfaccc63ea0fb824a59e0865dd19a13efee11
nourgmal <nouramohamedgamalll@gmail.com>
2023-01-02 20:28:22 +0200

    add implemntaion of Vectors

---
+++ inc/05_Vectors/Vectors.h
@@ -0,0 +1,17 @@
+#ifndef VECTORS_H_
+#define VECTORS_H_
+#include "STD_TYPES.h"
+
+typedef uint32_t VectorType;
+typedef struct
+{
+	VectorType CurrSize;
+	VectorType MaxSize;
+	VectorType* ptr;
+}vector_t;
+
+void Create(vector_t *V);
+void pushBack(vector_t *V,VectorType e);
+void popBack(vector_t *V);
+
+#endif /* VECTORS_H_ */
