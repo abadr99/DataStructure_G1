202e2e506ce205df3a3a1c50a727f2d1ccf615a5
Abdelrahman Badr <abdelrhmanezzbadr@outlook.com>
2023-02-03 13:30:01 +0000

    Adding CPP structure and makefile

---
+++ CPP/Stack/Stack.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Stack.h"
+
+void test () {
+    std::cout << "ASD";
+}
---
+++ CPP/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Stack.h"
+
+int main (int argc, char **argv) {
+    return 0;
+}
