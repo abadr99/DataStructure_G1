b631665f4799733c084aca74bc6d3db530147db5
noran97 <99215076+noran97@users.noreply.github.com>
2023-02-07 20:31:40 +0200

    PriorityQueue

---
+++ CPP/pri.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include <cassert>
+#include"prio.h"
+using namespace std;
+ PriorityQueue::PriorityQueue(): _size(0)
+{
+
+}
+ void PriorityQueue::Sort()
+{
+    for (int i = 0; i < _size- 1; i++)
+ {
+
+        for (int j = 0; j < _size - i - 1; j++)
+           {
+        if (_PqueueArr[j] > _PqueueArr[j + 1])
+        {
+          int a=_PqueueArr[j];
+           _PqueueArr[j]=_PqueueArr[j+1];
+           _PqueueArr[j+1]=a;
+        }
+
+}}
+}
+void PriorityQueue::Push(int element)
+{assert(_size < Max_Size && "YOU HAVE EXCEED THE EXPECTED  SIZE ");
+    _PqueueArr[_size] = element;
+    _size ++;
+    Sort();
+}
+
+PriorityQueue::GetTop()
+{
+    return _PqueueArr[0];
+}
+PriorityQueue::Pop()
+{
+    assert(_size != 0 && "PRIORITY QUEUE IS EMPTY");
+    _size --;
+    return _PqueueArr[_size];
+}
+PriorityQueue::GetSize()
+{
+    return _size;
+}
+
+void PriorityQueue::Clear() {
+    _size = 0;
+
+}
+void PriorityQueue::print()
+{
+    for(int i=0;i<_size;i++)
+    {
+        cout<<_PqueueArr[i]<<" ";
+    }
+}
