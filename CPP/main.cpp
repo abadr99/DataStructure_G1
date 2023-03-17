dcadf6bcb0ec891aea973727a970f66b515db0ca
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2023-03-17 23:10:01 +0200

    Adding Option Unit

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,38 +1,19 @@
 #include <iostream>
 #include <assert.h>
-#include "utils.h"
-#include "Stack.h"
-#include "BinaryTree.h"
-#include "BST.h"
+#include "Options/Options.h"
 
-void print(int& x) {
-    std::cout << x << " ";
-}
-using namespace DSA::DS::BINARY_TREE::BST;
+
 int main (int argc, char **argv) {
-    BST<int> B;
-    B.Insert(100);
-    B.Insert(70);
-    B.Insert(50);
-    B.Insert(80);
-    B.Insert(130);
-    B.Insert(110);
-    B.Insert(140);
-    B.Insert(120);
-    B.Insert(115);
-    B.Insert(125);
-    B.Insert(75);
-    B.Insert(71);
-    B.Insert(77);
-    B.InOrder(print);
-    std::cout<<"\n";
-    B.Delete(110);
-    B.InOrder(print);
-    std::cout<<"\n";
-    B.Delete(80);
-    B.InOrder(print);
-    std::cout<<"\n";
-    B.Delete(130);
-    B.InOrder(print);
-    std::cout<<"\n";
+    Options* Opt = Options::GetOptionsObj();
+    Opt->ParseOptions(argc,argv);
+    
+    if(Opt->IsExist("uTest") || Opt->IsExist("$u")) {
+        //RUN_UNIT_TESTS();
+    }
+
+    if(Opt->IsExist("iTest") || Opt->IsExist("$i")) {
+        //RUN_INTEGRATION_TESTS();
+    }
+
+
 }
