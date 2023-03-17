625f9dfdf0dea547cfa77854f7b84434f1c2534f
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2023-03-17 23:34:20 +0200

    Improve

--- CPP/SortingAlgrothims/Sort.cpp
+++ CPP/SortingAlgrothims/Sort.cpp
@@ -23,7 +23,33 @@ void SortingAlgrothim<T>::SelectionSort(T arr[], uint32_t size, SortingType_t So
         }        
     }
 }
-
+template<typename T>
+void SortingAlgrothim<T>::ShellSort(T arr[], uint32_t size, SortingType_t SortingType )
+{
+    for(uint32_t gap = size/2 ; gap > 0 ; gap/=2)
+    {
+        for(uint32_t Iterator1 = gap ; Iterator1 < size ; Iterator1++)
+        {
+            T tempIndex = arr[Iterator1];
+            uint32_t Iterator2 = 0;
+            if(SortingType == SortingType_t::ASCENDING)
+            {
+                for(Iterator2 = Iterator1 ; (Iterator2 >= gap) && (arr[Iterator2 - gap] > tempIndex) ; Iterator2-=gap)
+                {
+                    arr[Iterator2] = arr[Iterator2 - gap];
+                }
+            }
+            else if(SortingType == SortingType_t::DESCENDING)
+            {
+                for(Iterator2 = Iterator1 ; (Iterator2 >= gap) && (arr[Iterator2 - gap] < tempIndex) ; Iterator2-=gap)
+                {
+                    arr[Iterator2] = arr[Iterator2 - gap];
+                }
+            }
+            arr[Iterator2] = tempIndex;
+        }
+    }
+}
 template<typename T>
 uint32_t SortingAlgrothim<T>::GetMinIndex(T arr[], uint32_t FirstIndex, uint32_t LastIndex) {
     uint32_t Iterator = FirstIndex;
@@ -54,33 +80,6 @@ void SortingAlgrothim<T>::Swap(T& First,T& Second)
     Second = First - Second;
     First  = First - Second;
 }
-template<typename T>
-void SortingAlgrothim<T>::ShellSort(T arr[], uint32_t size, SortingType_t SortingType )
-{
-    uint32_t gap;
-    for(gap = size/2 ; gap>0 ; gap/=2)
-    {
-        for(uint32_t Iterator1 = gap ; Iterator1 < size ; Iterator1++)
-        {
-            T tempIndex = arr[Iterator1];
-            uint32_t Iterator2 = 0;
-            if(SortingType == SortingType_t::ASCENDING)
-            {
-                for(Iterator2 = Iterator1 ; (Iterator2 >= gap) && (arr[Iterator2 - gap] > tempIndex) ; Iterator2-=gap)
-                {
-                    arr[Iterator2] = arr[Iterator2 - gap];
-                }
-            }
-            else if(SortingType == SortingType_t::DESCENDING)
-            {
-                for(Iterator2 = Iterator1 ; (Iterator2 >= gap) && (arr[Iterator2 - gap] < tempIndex) ; Iterator2-=gap)
-                {
-                    arr[Iterator2] = arr[Iterator2 - gap];
-                }
-            }
-            arr[Iterator2] = tempIndex;
-        }
-    }
-}
+
 
 INSTANTIATE_CLASS_TEMPLATES(SortingAlgrothim);
